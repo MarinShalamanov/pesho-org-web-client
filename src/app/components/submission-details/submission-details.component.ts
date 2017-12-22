9245d6983f3713c1d1eafd533468a2ead4dfb2b1
Marin Shalamanov <shalamanov.marin@gmail.com>
2017-12-22 20:00:43 +0200

    submission details displays full information

--- src/app/components/submission-details/submission-details.component.ts
+++ src/app/components/submission-details/submission-details.component.ts
@@ -13,15 +13,6 @@ import { SubmissionsService } from '../../services/submissions.service';
   styleUrls: ['./submission-details.component.css']
 })
 export class SubmissionDetailsComponent implements OnInit {
-  testsDataSource = new ExampleDataSource();
-  displayedColumns = ['step', 'status', 'reason', 'time', 'input', 'output'];
-  id = 123;
-  time = '017-04-06 06:38:53 +0000 UTC';
-  problem = 'Kолежка';
-  language = 'java';
-  verdict = 'Accepted';
-  user = 'marin';
-  
   private contestId: string;
   private groupId: string;
   private submissionId: string;
@@ -41,44 +32,8 @@ export class SubmissionDetailsComponent implements OnInit {
       .getSubmission(this.submissionId)
       .subscribe(data => {
         this.submission = data as any;
+        console.log(this.submission);
       });
   }
   
-}
-
-export interface Element {
-  step: string;
-  status: string;
-  reason: string;
-  time: string;
-  input: string;
-  output: string;
-}
-
-const data: Element[] = [
-  {step: 'Test #1', status: 'OK', reason: '', time: '423', input: '', output: ''},
-  {step: 'Test #2', status: 'OK', reason: '', time: '423', input: '', output: ''},
-  {step: 'Test #3', status: 'OK', reason: '', time: '423', input: '', output: ''},
-  {step: 'Test #4', status: 'OK', reason: '', time: '423', input: '', output: ''},
-  {step: 'Test #5', status: 'Fail', reason: '', time: '423', input: '', output: ''},
-  {step: 'Test #6', status: 'OK', reason: '', time: '423', input: '', output: ''},
-  {step: 'Test #7', status: 'Fail', reason: '', time: '423', input: '', output: ''},
-  {step: 'Test #8', status: 'OK', reason: '', time: '423', input: '', output: ''},
-  {step: 'Test #9', status: 'OK', reason: '', time: '423', input: '', output: ''},
-  {step: 'Test #10', status: 'OK', reason: '', time: '423', input: '', output: ''},
-];
-
-/**
- * Data source to provide what data should be rendered in the table. The observable provided
- * in connect should emit exactly the data that should be rendered by the table. If the data is
- * altered, the observable should emit that new set of data on the stream. In our case here,
- * we return a stream that contains only one set of data that doesn't change.
- */
-export class ExampleDataSource extends DataSource<any> {
-  /** Connect function called by the table to retrieve one stream containing the data to render. */
-  connect(): Observable<Element[]> {
-    return Observable.of(data);
-  }
-
-  disconnect() {}
 }
